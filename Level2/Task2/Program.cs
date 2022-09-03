61267694110a199b98c888583bc63a2775b23887
Kirill Grigorev <grigorevkirill@mail.ru>
2022-09-03 14:34:58 +0300

    added Level2 and Tasks 1-4

---
+++ Level2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//================================================================
+// Задача 2 Дана сторона квадрата. Найти его периметр
+//================================================================
+
+Console.Write("Введите значение стороны квадрата: ");
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    double squareSide = double.Parse(inputLine);
+
+    if(squareSide > 0)
+    {
+        double perimeter = squareSide * 4;
+        Console.WriteLine($"Периметр квадрата: {perimeter}");
+    }
+    else 
+    {
+        Console.WriteLine("Error");
+    }
+}
---
+++ Level2/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿//================================================================
+// Задача 3 Дан радиус окружности. Найти ее диаметр. 
+//================================================================
+Console.Write("Введите радиус окружности: ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    double radius = double.Parse(inputLine);
+
+    if(radius > 0)
+    {
+        double diametr = radius * 2;
+        // Console.WriteLine($"Диаметр окружности равен: {diametr}");
+        Console.WriteLine("Диаметр окружности = {0}", diametr); // Еще один способ записи переменных
+    }
+    else
+    {
+        Console.WriteLine("Неправильный ввод");
+    }
+}
