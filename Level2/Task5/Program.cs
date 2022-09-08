d03a451d9944b26e0c53adc23c2623826bbdeceb
Kirill Grigorev <grigorevkirill@mail.ru>
2022-09-08 21:14:16 +0300

    added some new Tasks (Task5, Task6)

---
+++ Level2/Task5/Program.cs
@@ -0,0 +1,50 @@
+﻿int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+string ReadMathOperator(string line)
+{
+    Console.Write(line);
+    string mathOperator = Console.ReadLine() ?? "null";
+    return mathOperator;
+}
+
+double CalculateData(double a, double b, string c)
+{
+    string mathOperator = c;
+    double result = 0;
+
+    switch (mathOperator)
+    {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break;
+        case "*":
+            result = a * b;
+            break;
+        case "/":
+            result = a / b;
+            break;
+        default:
+            result = 0;
+            break;    
+    }
+    return result;
+}
+
+void Printdata(string line)
+{
+    Console.WriteLine(line);
+}
+
+int a = ReadData("Введите число 1: ");
+string mathOperator = ReadMathOperator("Введите мат. знак: ");
+int b = ReadData("Введите число 2: ");
+double result = CalculateData(a, b, mathOperator);
+Printdata($"Результат: {result}");
---
+++ Level2/Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = 3232134;
+char[] charArray = number.ToString().ToCharArray();
+Array.Reverse(charArray);
+char[] reversedaArray = charArray;
+Console.WriteLine(reversedaArray);
+int b = int.Parse(reversedaArray);
+Console.WriteLine(b);
+
+
+if(b == number)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
